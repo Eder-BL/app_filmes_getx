457c146a9a1d235fed2ef91bb2ec763c14c2b352
Eder-BL <eder.bergmann@gmail.com>
2022-11-20 17:04:10 -0300

    [FIX] Implementando login com google

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_filmes/application/bindings/application_bindings.dart';
 import 'package:app_filmes/modules/login/login_module.dart';
 import 'package:app_filmes/modules/splash/splash/splash_module.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -18,10 +19,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      initialBinding: ApplicationBindings(),
       getPages: [
         ...SplashModule().routers,
         ...LoginModule().routers,
--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,18 +1,46 @@
+import 'dart:developer';
+
 import 'package:app_filmes/application/ui/loader/loader_mixin.dart';
+import 'package:app_filmes/application/ui/messages/messages_mixin.dart';
+import 'package:app_filmes/services/login/login_service.dart';
 import 'package:get/get.dart';
 
-class LoginController extends GetxController with LoaderMixin {
+class LoginController extends GetxController with LoaderMixin, MessagesMixin {
+  final LoginService _loginService;
   final loading = false.obs;
+  final message = Rxn<MessageModel>();
+
+  LoginController({required LoginService loginService})
+      : _loginService = loginService;
 
   @override
   void onInit() {
     loaderListener(loading);
+    messageListener(message);
     super.onInit();
   }
 
   Future<void> login() async {
-    loading(true);
-    await Future.delayed(const Duration(seconds: 2));
-    loading.value = false;
+    try {
+      loading(true);
+      await _loginService.login();
+      loading(false);
+      message(
+        MessageModel.info(
+          title: 'Sucesso',
+          message: 'Login realizado com sucesso!',
+        ),
+      );
+    } catch (e, s) {
+      log(e.toString(), name: 'login()');
+      log(s.toString(), name: 'login()');
+      loading(false);
+      message(
+        MessageModel.erro(
+          title: 'Login error',
+          message: 'Ocorreu um erro ao tentar realizar o Login',
+        ),
+      );
+    }
   }
 }
